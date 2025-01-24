f48f5d982c09cd91118440ab4d110a28cff6609f
gyeonwoo-jerry <gyeonwoo2916@google.com>
2025-01-24 11:01:27 +0900

    first commit

---
+++ src/main/java/easylab/easylab/EasylabApplication.java
@@ -0,0 +1,13 @@
+package easylab.easylab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasylabApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(EasylabApplication.class, args);
+  }
+
+}
---
+++ src/test/java/easylab/easylab/EasylabApplicationTests.java
@@ -0,0 +1,13 @@
+package easylab.easylab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasylabApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
