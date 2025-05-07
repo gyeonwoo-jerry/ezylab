e673f03a0b69d7a30cfeb17c125d0273b765f525
gyeonwoo-jerry <gyeonwoo2916@google.com>
2025-05-07 19:06:49 +0900

    [feat] 일부 수정

--- src/frontend/front/src/setupProxy.js
+++ src/frontend/front/src/setupProxy.js
@@ -4,10 +4,13 @@ module.exports = function (app) {
   const apiUrl = "http://211.110.44.79:48080"; // 기본 포트 설정
 
   app.use(
-    "/api",
-    createProxyMiddleware({
-      target: apiUrl,
-      changeOrigin: true,
-    })
+      "/api", // /api로 시작하는 모든 요청을 백엔드 서버로 프록시
+      createProxyMiddleware({
+        target: apiUrl,
+        changeOrigin: true, // CORS 우회용
+        pathRewrite: {
+          '^/api': '', // `/api`를 타겟 서버에 맞게 경로를 수정하려면 여기를 추가할 수 있음
+        },
+      })
   );
 };
