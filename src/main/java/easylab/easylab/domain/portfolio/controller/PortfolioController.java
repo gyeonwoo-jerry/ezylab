fc0bc40250ef332510d5804ce538b69097271c7d
gyeonwoo-jerry <gyeonwoo2916@google.com>
2025-06-10 13:58:42 +0900

    [feat] 포트폴리오 리스폰스 수정

--- src/main/java/easylab/easylab/domain/portfolio/controller/PortfolioController.java
+++ src/main/java/easylab/easylab/domain/portfolio/controller/PortfolioController.java
@@ -1,7 +1,6 @@
 package easylab.easylab.domain.portfolio.controller;
 
 import easylab.easylab.domain.auth.resolver.AuthenticationUserId;
-import easylab.easylab.domain.board.dto.BoardResponseDto;
 import easylab.easylab.domain.common.response.ApiResponse;
 import easylab.easylab.domain.common.response.PageResponse;
 import easylab.easylab.domain.portfolio.dto.PortfolioRequestDto;
@@ -12,15 +11,11 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Page;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestPart;
